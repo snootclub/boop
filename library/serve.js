69ab781401e91ea1ced2ab4862448eda4a8d7e41
chee <chee@snoot.club>
2020-11-01 13:34:30 +0000

    Update serve.js

--- library/serve.js
+++ library/serve.js
@@ -2,6 +2,15 @@ let serveHandler = require("serve-handler")
 
 module.exports = async ({request, response, websiteDirectory}) =>
 	serveHandler(request, response, {
+		headers: [
+			{
+				source: "**/*.@(org)",
+				headers: [{
+					key: "content-type",
+					value: "text/org"
+				}]
+			}
+		],
 		public: websiteDirectory,
 		directoryListing: false,
 		cleanUrls: true,
